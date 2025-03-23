ef35d19d105dae16ebc7eb950429fcf474935cda
iamoliver <wonderoliver155@gmail.com>
2025-03-23 15:56:47 +0300

    starting afresh

---
+++ Javascript/app.js
@@ -0,0 +1,7 @@
+const name = "Simon Oliver"
+
+function upperCase(text) {
+     const result = text.toUpperCase();
+     console.log(result)
+}
+upperCase(name);
---
+++ Javascript/board.py
@@ -0,0 +1,5 @@
+oranges = 5
+mangoes = 7
+apples = 3
+
+print(oranges >2)

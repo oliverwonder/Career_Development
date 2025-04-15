2c2dea975f90c413e4e822b5d012ea2506b411bc
oliver <wonderoliver155@gmail.com>
2025-04-15 10:57:29 +0300

    created a html file, linked a test.js file to start learning gontol flow and conditionals in Javascript

---
+++ Rough Work/test.js
@@ -0,0 +1,12 @@
+var youLikeMeat = true;
+if (youLikeMeat){
+  document.write("Here is the meat menu for you to choose")
+}
+
+var myNum = 4;
+if (myNum > 5){
+  document.write(" and your number is greater than 5");
+}
+else{
+  document.write(" and your number is not greater than 5")
+}
